8ea0b2f6fee1e8996d75ea5151e1b6760008c92f
Adrian Ozuna <silkbirch06@gmail.com>
2022-07-27 15:06:33 -0400

    Project structure for development

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const dotenv = require('dotenv')
+const morgan =  require('morgan')
+const exphbs = require('express-handlebars')
+
+// Load config
+dotenv.config({ path: './config/config.env' })
+
+const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+app.engine('.hbs', exphbs.engine({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+app.use('/', require('./routes/index'))
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}!`);
+});
+
